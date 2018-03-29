62cafebb77f11c6222624b57171f92488e662cbb
Sohan Shirodkar <sohan1998@github.com>
2018-03-29 16:03:36 +0530

    Updated Variables

--- flappybird.py
+++ flappybird.py
@@ -9,9 +9,48 @@ from pygame.locals import *
 
 
 
-SCREEN_DIMENSIONS = screenX, screenY = (720, 480)
+SCREEN_DIMENSIONS = screenX, screenY = (288, 512)
 BG_COLOR = (0, 0, 0)
 FPS = 60
+PIPE_GAP = 100
+
+# list of all possible players (tuple of 3 positions of flap)
+PLAYERS_LIST = (
+    # red bird
+    (
+        'assets/sprites/redbird-upflap.png',
+        'assets/sprites/redbird-midflap.png',
+        'assets/sprites/redbird-downflap.png',
+    ),
+    # blue bird
+    (
+        # amount by which base can maximum shift to left
+        'assets/sprites/bluebird-upflap.png',
+        'assets/sprites/bluebird-midflap.png',
+        'assets/sprites/bluebird-downflap.png',
+    ),
+    # yellow bird
+    (
+        'assets/sprites/yellowbird-upflap.png',
+        'assets/sprites/yellowbird-midflap.png',
+        'assets/sprites/yellowbird-downflap.png',
+    ),
+)
+
+# list of backgrounds
+BACKGROUNDS_LIST = (
+    'assets/sprites/background-day.png',
+    'assets/sprites/background-night.png',
+)
+
+# list of pipes
+PIPES_LIST = (
+    'assets/sprites/pipe-green.png',
+    'assets/sprites/pipe-red.png',
+)
+
+
+
 
 
 pygame.init()
@@ -26,6 +65,7 @@ clock = pygame.time.Clock()
 
 
 def main():
+
 	while True:
 
 		for event in pygame.event.get():
